ea16f92f04e8f4e3d965629221e0a8fa2e17c77e
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-05-09 23:16:23 +0900

    関数とグローバル変数の例  関数名を例示用に修正

--- global_variable_in_func.py
+++ global_variable_in_func.py
@@ -1,22 +1,22 @@
-def dfs(v):
+def lower_func(v):
 	w = match[v]
 	if(w < 0):
 		match[v] = v+1
-		print(match)
+		print("lower_func:" , match)
 	return True
 
-def bipartite_matching():
+def higher_func():
 	match = [-1] * V
 	for v in range(V):
-		dfs(v)
-		print(match)
+		lower_func(v)
+		print("higher_func:", match)
 	return 
 
-V = 5
-match = [-2] * V #マッチングのペア
+V = 3
+match = [-1] * V
 
-bipartite_matching()
-print(match) # dfs関数によって、書き換わっている
+higher_func()
+print("global:", match) # dfs関数によって、書き換わっている
 
-# 疑問1 dfs関数の中のmatchはグローバル変数を指しているっぽい。それはどうして? 関数呼び出しを順にたどるならbipartite_matchingの中のmatchじゃないか?
+# 疑問1 dfs関数の中のmatchはグローバル変数を指しているっぽい。それはどうして? 関数呼び出しを順にたどるならhigher_funcの中のmatchじゃないか?
 # 疑問2 dfs関数の中のmatchはグローバル変数を指しているっぽい。それはどうして? あとで値の代入が発生しているならローカル変数じゃないか?
