67e2cd9850ab5c23a2fb2c44660c1e642bb0dd1a
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-05-11 15:05:12 +0900

    id部分を追加

--- global_variable_in_func.py
+++ global_variable_in_func.py
@@ -2,7 +2,8 @@ def lower_func(v):
 	w = match[v]
 	if(w < 0):
 		match[v] = v+1
-		print("lower_func:" , match)
+		print("lower_func:", match)
+		print("lower_func:", id(match))
 	return True
 
 def higher_func():
@@ -10,13 +11,15 @@ def higher_func():
 	for v in range(V):
 		lower_func(v)
 		print("higher_func:", match)
+		print("higher_func:", id(match))
 	return 
 
 V = 3
 match = [-1] * V
 
 higher_func()
-print("global:", match) # dfs関数によって、書き換わっている
+print("global:", match)
+print("global:", id(match))
 
 # 疑問1 dfs関数の中のmatchはグローバル変数を指しているっぽい。それはどうして? 関数呼び出しを順にたどるならhigher_funcの中のmatchじゃないか?
 # 疑問2 dfs関数の中のmatchはグローバル変数を指しているっぽい。それはどうして? あとで値の代入が発生しているならローカル変数じゃないか?
