23a154667f483e53730b732a001b3daa385673ba
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2022-01-04 18:14:44 +0900

    最初のkivyアプリケーション（Hello world）

---
+++ kivy_tutorial/hello_world/main.py
@@ -0,0 +1,14 @@
+# https://kivy.org/doc/stable/guide/basic.html#quickstart
+import kivy
+kivy.require('2.0.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello World')
+
+if __name__ == '__main__':
+    MyApp().run()
